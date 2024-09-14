3648cbdfbcabd147cb4d4cd1159c4487c9da66e2
yashwanth <pyashwanth14@gmail.com>
2024-09-14 20:58:06 +0530

    feature

--- src/com/programming/Programs.java
+++ src/com/programming/Programs.java
@@ -3,7 +3,7 @@ package com.programming;
 public class Programs {
 	public static void main(String args[]) {
 		
-		System.out.println("Hii java8");
+		System.out.println("Hii java8 on feature");
 		
 		
 	}
